2f71e4c2ee7efc0489dc972d9cf4a9c0a774f2bf
ideveshchoudhary <deveshch2@gmail.com>
2024-05-03 07:57:02 +0530

    created fun of signin page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,4 +9,49 @@ export class LoginComponent {
 
   isRightDivShown: boolean = false;
 
+  signUpObj: SignUpModel = new SignUpModel();
+
+  loginObj: LoginModel = new LoginModel();
+
+  onRegister() {
+    debugger
+    const localuser = localStorage.getItem("signinUsers") //key for storing data in localstorage
+    if (localuser != null) {
+      const storeuser = JSON.parse(localuser);
+      storeuser.push(this.signUpObj);
+      localStorage.setItem("signinUsers", JSON.stringify(storeuser))
+      alert("Register Succsfull")
+    }
+    else {
+      const storeuser = [];
+      storeuser.push(this.signUpObj);
+      localStorage.setItem("signupForm", JSON.stringify(storeuser))
+    }
+  }
+
 }
+
+export class SignUpModel {
+  name: string;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.name = "";
+    this.email = "";
+    this.password = "";
+  }
+}
+
+export class LoginModel {
+  email: string;
+  password: string;
+
+  constructor() {
+    this.email = "";
+    this.password = "";
+  }
+}
+
+
+
